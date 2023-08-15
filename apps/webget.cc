5cc97894eb25a480a6837297db25026f701570e7
LRL52 <525687841@qq.com>
2023-08-15 20:50:35 +0800

    修改 webget 为 CS144TCPSocket

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,4 +1,4 @@
-#include "socket.hh"
+#include "tcp_sponge_socket.hh"
 #include "util.hh"
 
 #include <cstdlib>
@@ -17,7 +17,7 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    TCPSocket sock;
+    CS144TCPSocket sock;
     sock.connect(Address(host, "http"));
     sock.write("GET " + path + " HTTP/1.1\r\n");
     sock.write("Host: " + host + "\r\n");
@@ -29,8 +29,9 @@ void get_URL(const string &host, const string &path) {
         cout << sock.read();
     }
     sock.close();
-
+    
     cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    sock.wait_until_closed();
     // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
